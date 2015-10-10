4c715068bbb7c35bc522b82f76c79aa71c60a0f1
Yang Zhang <albert.zhang@jdycar.cn>
2015-10-10 18:35:39 +0800

    Added a parameter changes so that user can turn on/off the insert/update/delete

--- Source/DATAFilter.h
+++ Source/DATAFilter.h
@@ -1,5 +1,12 @@
 @import CoreData;
 
+typedef NS_OPTIONS(NSUInteger, DATAFilterChangOperations) {
+    DATAFilterChangOperationInsert = 1 << 0,
+    DATAFilterChangOperationUpdate = 1 << 1,
+    DATAFilterChangOperationDelete = 1 << 2,
+    DATAFilterChangOperationAll = 0xFFFFFFFF
+};
+
 @interface DATAFilter : NSObject
 
 + (void)changes:(NSArray *)changes
@@ -11,6 +18,26 @@
         updated:(void (^)(NSDictionary *objectJSON, NSManagedObject *updatedObject))updated;
 
 + (void)changes:(NSArray *)changes
+  inEntityNamed:(NSString *)entityName
+       localKey:(NSString *)localKey
+      remoteKey:(NSString *)remoteKey
+        context:(NSManagedObjectContext *)context
+      predicate:(NSPredicate *)predicate
+       inserted:(void (^)(NSDictionary *objectJSON))inserted
+        updated:(void (^)(NSDictionary *objectJSON, NSManagedObject *updatedObject))updated;
+
+
++ (void)changes:(NSArray *)changes
+     operations:(DATAFilterChangOperations)operations
+  inEntityNamed:(NSString *)entityName
+       localKey:(NSString *)localKey
+      remoteKey:(NSString *)remoteKey
+        context:(NSManagedObjectContext *)context
+       inserted:(void (^)(NSDictionary *objectJSON))inserted
+        updated:(void (^)(NSDictionary *objectJSON, NSManagedObject *updatedObject))updated;
+
++ (void)changes:(NSArray *)changes
+     operations:(DATAFilterChangOperations)operations
   inEntityNamed:(NSString *)entityName
        localKey:(NSString *)localKey
       remoteKey:(NSString *)remoteKey
